e558c7ed55b5e086218ed71009d0bce5b3cd608a
Jared Palmer <jaredpalmer369@gmail.com>
2025-06-06 15:27:33 -0500

    Fix Supabase client to use createPagesBrowserClient

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,4 +3,4 @@
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/types/supabase'
 
-export const supabase = createBrowserSupabaseClient<Database>()
+export const supabase = createPagesBrowserClient<Database>()
