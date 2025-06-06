8214a449ccb5a8c9ec879185b7a3634e95b2d19d
Jared Palmer <jaredpalmer369@gmail.com>
2025-06-06 15:08:40 -0500

    Refactor: use createPagesBrowserClient instead of deprecated client

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 'use client'
 
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/types/supabase'
 
 export const supabase = createBrowserSupabaseClient<Database>()
