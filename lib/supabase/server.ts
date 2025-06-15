3f332e39022b9b733ec811c8e86927eed85e76b5
Jared Palmer <jaredpalmer369@gmail.com>
2025-06-15 16:02:15 -0500

    Fix Supabase SSR integration in login route

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,12 +1,15 @@
+'use server';
+
 import { cookies } from 'next/headers';
 import { createServerClient } from '@supabase/ssr';
 import { Database } from '@/types/supabase';
 
-export const createClient = () =>
-  createServerClient<Database>(
+export const createClient = () => {
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies,
     }
   );
+};
