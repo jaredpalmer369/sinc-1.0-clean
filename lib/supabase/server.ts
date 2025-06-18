9571dc63dda88c3c1f8adeebebe8a776e79fee92
Jared Palmer <jaredpalmer369@gmail.com>
2025-06-18 15:22:54 -0500

    fix: update server.ts to use correct Supabase cookie interface

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,10 +1,23 @@
 import { cookies } from 'next/headers'
 import { createServerClient } from '@supabase/ssr'
-import { type Database } from '@/types/supabase'
+import type { Database } from '@/types/supabase'
 
-export const createClient = () =>
-  createServerClient<Database>(
+export function createClient() {
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies }
+    {
+      cookies: {
+        get(name) {
+          return cookies().get(name)?.value
+        },
+        set(name, value, options) {
+          cookies().set({ name, value, ...options })
+        },
+        remove(name, options) {
+          cookies().delete({ name, ...options })
+        },
+      },
+    }
   )
+}
