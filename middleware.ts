879ef47eddd5f31e515ea604f129c1fd2927e100
Jared Palmer <jaredpalmer369@gmail.com>
2025-06-18 12:38:27 -0500

    fix: update middleware to use supabase/ssr

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,17 @@
-import { createMiddlewareClient } from '@supabase/ssr'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { createMiddlewareClient } from '@supabase/ssr';
+import { type NextRequest, NextResponse } from 'next/server';
+import type { Database } from '@/types/supabase';
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
-  await supabase.auth.getSession()
-  return res
+  const res = NextResponse.next();
+
+  const supabase = createMiddlewareClient<Database>({ req, res });
+
+  await supabase.auth.getSession(); // ensures session cookie is handled
+
+  return res;
 }
 
 export const config = {
   matcher: ['/dashboard/:path*', '/prompts/:path*', '/signout'],
-}
+};
