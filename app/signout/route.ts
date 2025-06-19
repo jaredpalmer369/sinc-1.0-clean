8336ca686292b67ae7bb03b9936819251b26daeb
Jared Palmer <jaredpalmer369@gmail.com>
2025-06-18 22:42:09 -0500

    Fix signout route to support GET requests

--- app/signout/route.ts
+++ app/signout/route.ts
@@ -1,29 +1,27 @@
-import { NextResponse } from 'next/server'
-import { createServerClient } from '@supabase/ssr'
-import type { Database } from '@/types/supabase'
+import { createServerClient } from '@supabase/ssr';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import type { Database } from '@/types/supabase';
 
-export const dynamic = 'force-dynamic'
-
-export async function POST() {
-  const res = NextResponse.redirect(`${process.env.NEXT_PUBLIC_SITE_URL}/login`, {
-    status: 302,
-  })
-
+export async function GET(req: NextRequest) {
+  const res = NextResponse.redirect(`${process.env.NEXT_PUBLIC_SITE_URL}/login`);
   const supabase = createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll: () => [],
-        setAll: (cookies) => {
-          cookies.forEach(({ name, value, options }) =>
+        getAll() {
+          return req.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
             res.cookies.set(name, value, options)
-          )
+          );
         },
       },
     }
-  )
+  );
 
-  await supabase.auth.signOut()
-  return res
+  await supabase.auth.signOut();
+  return res;
 }
