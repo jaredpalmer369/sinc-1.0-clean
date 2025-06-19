9e3d1ad5c214ba96ce9fea86da78d8958ffa3df0
Jared Palmer <jaredpalmer369@gmail.com>
2025-06-18 22:46:32 -0500

    Fix: support GET signout route

--- app/signout/route.ts
+++ app/signout/route.ts
@@ -1,27 +1,28 @@
-import { createServerClient } from '@supabase/ssr';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import type { Database } from '@/types/supabase';
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import type { Database } from '@/types/supabase'
 
 export async function GET(req: NextRequest) {
-  const res = NextResponse.redirect(`${process.env.NEXT_PUBLIC_SITE_URL}/login`);
+  const res = NextResponse.redirect(`${process.env.NEXT_PUBLIC_SITE_URL}/login`)
+
   const supabase = createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
-          return req.cookies.getAll();
+          return req.cookies.getAll()
         },
         setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value, options }) =>
             res.cookies.set(name, value, options)
-          );
+          )
         },
       },
     }
-  );
+  )
 
-  await supabase.auth.signOut();
-  return res;
+  await supabase.auth.signOut()
+  return res
 }
